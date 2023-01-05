dbe6057bb571035b3d5d3c175933f31766e9abf4
Ali Padron <alijesus29@gmail.com>
2023-01-05 09:48:25 -0400

    feat: la configuración se convierte a un archivo .js para poder extenderlo en otros proyectos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "printWidth": 80,
   "tabWidth": 2,
   "useTabs": false,
@@ -11,4 +11,5 @@
   "bracketSameLine": false,
   "arrowParens": "always",
   "endOfLine": "lf"
-}
+};
+
